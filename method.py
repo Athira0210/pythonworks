c4ca7edfd558810ab0d5bd6b67730901fbc75082
Athira0210 <106605121+Athira0210@users.noreply.github.com>
2022-06-21 11:24:45 +0530

    pyfun

---
+++ method.py
@@ -0,0 +1,13 @@
+def max_fun(*args):
+    return max(args)
+
+print(max_fun(10,20))
+
+def min_fun(*args):
+    return min(args)
+print(min_fun(10,20,30))
+
+def add(*args):
+    return sum(args)
+
+print(add(5,5))
