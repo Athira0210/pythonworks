a63ebc26eeb71520c4d5b1133d50d90faddb73ca
Athira0210 <106605121+Athira0210@users.noreply.github.com>
2022-07-07 11:26:56 +0530

    funnew

--- funpro.py
+++ funpro.py
@@ -1,3 +1,4 @@
+from functools import reduce
 lst=[1,2,3,4,6]
 
 # squares=list(map(lambda n:n**2,lst))
@@ -7,3 +8,22 @@ lst=[1,2,3,4,6]
 
 num_out=list(map(lambda n:n-1 if n<5 else n+1,lst))
 print(num_out)
+
+evens=list(filter(lambda n:n%2==0,lst))
+print(evens)
+
+gt_ten=list(filter(lambda n:n>2,lst))
+print(gt_ten)
+
+sum=reduce(lambda n1,n2:n1+n2,lst)
+print(sum)
+
+maximum=reduce(lambda n1,n2:n1 if n1>n2 else n2,lst)
+print(maximum)
+
+minimum=reduce(lambda n1,n2:n1 if n1<n2 else n2,lst)
+print(minimum)
+
+product=reduce(lambda n1,n2:n1*n2,lst)
+print(product)
+
